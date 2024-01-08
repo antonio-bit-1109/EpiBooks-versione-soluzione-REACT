e35f892459230dbc28985b89ea0b4c34baec5a08
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-01-08 17:14:49 +0100

    gg

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,41 +1,40 @@
-import { Component } from 'react'
-import SingleBook from './SingleBook'
-import { Col, Form, Row } from 'react-bootstrap'
+import { Component } from "react";
+import SingleBook from "./SingleBook";
+import { Col, Form, Row } from "react-bootstrap";
 
 class BookList extends Component {
-  state = {
-    searchQuery: '',
-  }
+    state = {
+        searchQuery: "",
+    };
 
-  render() {
-    return (
-      <>
-        <Row className="justify-content-center mt-5">
-          <Col xs={12} md={4} className="text-center">
-            <Form.Group>
-              <Form.Control
-                type="search"
-                placeholder="Cerca un libro"
-                value={this.state.searchQuery}
-                onChange={(e) => this.setState({ searchQuery: e.target.value })}
-              />
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row className="g-2 mt-3">
-          {this.props.books
-            .filter((b) =>
-              b.title.toLowerCase().includes(this.state.searchQuery)
-            )
-            .map((b) => (
-              <Col xs={12} md={4} key={b.asin}>
-                <SingleBook book={b} />
-              </Col>
-            ))}
-        </Row>
-      </>
-    )
-  }
+    render() {
+        return (
+            <>
+                <Row className="justify-content-center mt-5">
+                    <Col xs={12} md={4} className="text-center">
+                        <Form.Group>
+                            <Form.Control
+                                type="search"
+                                placeholder="Cerca un libro"
+                                value={this.state.searchQuery}
+                                onChange={(e) => this.setState({ searchQuery: e.target.value })}
+                            />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row className="g-2 mt-3">
+                    {this.props.books
+                        .filter((b) => b.title.toLowerCase().includes(this.state.searchQuery))
+                        .slice(0, 6)
+                        .map((b) => (
+                            <Col xs={12} md={4} key={b.asin}>
+                                <SingleBook book={b} />
+                            </Col>
+                        ))}
+                </Row>
+            </>
+        );
+    }
 }
 
-export default BookList
+export default BookList;
